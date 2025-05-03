b078ffacb1cb10697db4270569a13986bc64bd53
Ali Hassan <mrkhan111243@gmail.com>
2025-05-03 22:09:26 +0500

    Initial commit

--- loan/loan/settings.py
+++ loan/loan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nm0+ecmo2x-2lf&2a!p$v!0anjs_178h41(4@7%oy$a=n6)8uv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
